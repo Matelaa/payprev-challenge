211a136770e8975a5a5a9b1ddcea2c5a13fbbea0
José Matela <jmatelaneto@gmail.com>
2019-08-29 02:05:57 -0300

    Showing which port is running the api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 const server = require('./server');
 
-server.listen(process.env.PORT || 3333);
+server.listen(process.env.PORT || 3333, () => {
+    console.log(`Listening on ${process.env.PORT}`)
+});
