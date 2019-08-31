0cb742010309cb2d6110ea3a0cd60642def0c1e1
José Matela <jmatelaneto@gmail.com>
2019-08-30 22:47:04 -0300

    Creating a method to create a userGit in database

--- src/controllers/UserAdminController.js
+++ src/controllers/UserAdminController.js
@@ -22,5 +22,33 @@ module.exports = {
                                                bio,
                                                location,
                                                html_url })
+    },
+
+    async store(req, res) {
+        const { isAdmin } = await User.findById({ _id: req.user._id })
+        if (!isAdmin) {
+            return res.status(401).json({ error: 'You dont have permission to do this.' })
+        }
+
+        const { username } = req.params
+        
+        const userGit = await axios.get(`https://api.github.com/users/${username}`)
+        if (!userGit) {
+            return res.status(404).json({ error: 'This user does not exist in our database.' })
+        }
+
+        const userExists = await UserGit.findOne({ login: username })
+        if (userExists) {
+            return res.status(401).json({ error: 'This user already exists in our database.' })
+        }
+
+        const { login, name, bio, location, html_url } = userGit.data
+        const user = await UserGit.create({ login,
+                                            name,
+                                            bio,
+                                            location,
+                                            html_url })
+
+        return res.status(201).json(user)
     }
 }
