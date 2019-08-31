b8fc77fe806622be3614769017934540c3f9e355
José Matela <jmatelaneto@gmail.com>
2019-08-30 22:52:56 -0300

    Deleting the login method from UserController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -54,30 +54,5 @@ module.exports = {
                                          password: cryptPassword })
 
         return res.status(201).json(user)
-    },
-
-    async login(req, res) {
-        const schema = Yup.object().shape({
-            email: Yup.string().email().required(),
-            password: Yup.string().required().min(6)
-        });
-
-        if (!(await schema.isValid(req.body))) {
-            return res.status(400).json({ error: `Invalid email or password!` })
-        }
-
-        const { email, password } = req.body;
-
-        const user = await User.findOne({ email })
-        if (!user) {
-            return res.status(404).json({ error: 'This user does not exist in our database.'  })
-        }
-
-        const checkPassword = await bcrypt.compare(password, user.password)
-        if (!checkPassword) {
-            return res.status(400).json({ error: 'Wrong password.' })
-        }
-
-        return res.status(200).json({ success : user })
     }
 }
