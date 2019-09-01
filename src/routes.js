f95fdddca103e229386ab57028354337c711cf64
José Matela <jmatelaneto@gmail.com>
2019-08-31 21:49:18 -0300

    Chaging the order of routes

--- src/routes.js
+++ src/routes.js
@@ -9,10 +9,6 @@ const authMiddleware = require('./middlewares/auth');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    res.json('Hello Payprev')
-})
-
 // UserController - Rotas para criar um User e listar os users.
 routes.post('/user', UserController.store)
 routes.get('/users', UserController.index)
